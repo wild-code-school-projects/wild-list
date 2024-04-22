7f67447564b320d16f6a9177c59b2ba7906d4edf
SuperCoolNinja <sbpro570@gmail.com>
2024-04-22 17:20:31 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fruits = new List<string>();
+            fruits.Add("Banana");
+            fruits.Add("Orange");
+            fruits.Add("Apple");
+
+
+
+            Console.WriteLine("Before : ");
+
+            foreach (var fruit in fruits)
+                Console.WriteLine($"\t{fruit}");
+
+            fruits.RemoveAt(0);
+
+            fruits.Remove("Apple");
+
+            fruits[0] = "Strawberry";
+
+            Console.WriteLine("-----------------------");
+
+
+            Console.WriteLine("After : ");
+
+
+            foreach (var fruit in fruits)
+                Console.WriteLine($"\t{fruit}");
+        }
+    }
+}
